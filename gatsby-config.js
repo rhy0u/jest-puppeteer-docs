66dcb5f90bcbf34290024af711f725fcbfce3742
Greg Bergé <berge.greg@gmail.com>
2018-11-22 12:12:55 +0100

    chore: fix build

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,6 +98,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
+        sitemap: null,
+        host: null,
         policy: [{ userAgent: '*', disallow: '/' }],
       },
     },
